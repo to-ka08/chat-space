a81fa42b10c0d833b18374f5f493fef92823a819
T.K <t.k@TKnoMacBook-Air.local>
2018-02-22 20:10:51 +0900

    add chat member when click

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,7 @@
 $(function() {
 
 var user_list = $('#user-search-result');
+var member_list = $('#chat-group-users');
 
 function appendUser(user) {
   var html = `<div class="chat-group-user clearfix">
@@ -13,6 +14,15 @@ function appendUser(user) {
 function appendNoUser(user) {
   var html = `<div class="chat-group-user clearfix">${user}</div>`
   user_list.append(html);
+}
+
+function addUser(id, name) {
+  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                <input name='group[user_ids][]' type='hidden' value='${id}'>
+                <p class='chat-group-user__name'>${name}</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`
+  member_list.append(html);
 }
 
   $('#user-search-field').on('keyup', function() {
@@ -39,7 +49,9 @@ function appendNoUser(user) {
     })
   });
 
-  $(document).on('click', '.user-search-add.chat-group-user__btn.chat-group-user__btn--add', function() {
-    console.log('aa');
+  $(document).on('click', '.chat-group-user__btn--add', function() {
+    var id = $(this).data('user-id');
+    var name = $(this).data('user-name');
+    addUser(id, name);
   });
 });
