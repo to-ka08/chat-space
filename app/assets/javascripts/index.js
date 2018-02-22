a786b47299d30980c23ff8fe487c71494b0053d2
T.K <t.k@TKnoMacBook-Air.local>
2018-02-22 17:04:08 +0900

    add html in view

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,8 +1,24 @@
 $(function() {
+
+var user_list = $('#chat-group-users');
+
+function appendUser(user) {
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+              </div>`
+  user_list.append(html);
+}
+
+function appendNoUser(user) {
+  var html = `<div class="chat-group-user clearfix">${user}</div>`
+  user_list.append(html);
+}
+
   $('#user-search-field').on('keyup', function() {
     var input = $('#user-search-field').val();
     $.ajax({
-      url: '/user/index',
+      url: '/users',
       type: 'GET',
       data: { keyword: input },
       dataType: 'json'
